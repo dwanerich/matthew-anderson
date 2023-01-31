f122be355b14ade04959a629e3b670affdcb4ab1
Dwane Richards <drichards@pagerduty.com>
2023-01-30 20:11:52 -0500

    created contact form UI

---
+++ src/Work.js
@@ -0,0 +1,29 @@
+import Masonry from "react-responsive-masonry"
+import { ResponsiveMasonry } from "react-responsive-masonry";
+import { images } from './portfolio/index'
+
+
+
+// images = [
+//   "https://drive.google.com/file/d/1Db9uTAG0sqCK5KIqY4PWTU8LqgYdlPzF/view?usp=share_link",
+//   "https://cdn.pixabay.com/photo/2020/11/08/09/41/deer-5723225_1280.jpg",
+//   "https://images.unsplash.com/photo-1661961110372-8a7682543120?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+// ]
+
+const columnsCountBreakPoints = { 350: 1, 750: 2, 900: 3, 1400: 2 };
+
+
+
+const Work = () =>{
+  return (
+    <div class="container">
+      <ResponsiveMasonry columnsCountBreakPoints={columnsCountBreakPoints}>
+  <Masonry gutter={4}>
+    {images.map((image) => (
+      <img src={image} alt='alt'/>
+    ))}
+  </Masonry>
+</ResponsiveMasonry>
+    </div>
+        )}
+        export default Work;
---
+++ src/portfolio/index.js
@@ -0,0 +1,39 @@
+import PNG1  from "./1.PNG"
+import PNG2  from "./2.PNG"
+import PNG3  from "./3.PNG"
+import PNG4  from "./4.PNG"
+import PNG5 from "./5.PNG"
+import PNG6  from "./6.PNG"
+import PNG7  from "./7.PNG"
+import PNG8  from "./8.PNG"
+import PNG9  from "./9.PNG"
+import PNG10  from "./10.PNG"
+import PNG11 from "./11.PNG"
+import PNG12 from "./12.PNG"
+
+export const images = [
+  PNG1,
+  PNG2,
+  PNG3,
+  PNG4,
+  PNG5,
+  PNG6,
+  PNG7,
+  PNG8,
+  PNG9,
+  PNG10,
+  PNG11,
+  PNG12,
+  PNG1,
+  PNG2,
+  PNG3,
+  PNG4,
+  PNG5,
+  PNG6,
+  PNG7,
+  PNG8,
+  PNG9,
+  PNG10,
+  PNG11,
+  PNG12
+]
